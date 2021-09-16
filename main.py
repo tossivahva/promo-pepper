f9918d3c33fd4b7b7722068f57f0460b6fd039be
Nikita Zhuravlev <juravlev.nv@gmail.com>
2021-09-16 11:16:46 +0300

    Added Kivy library, created base App, set up Kivy file

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
+import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+kivy.require('2.0.0')
 
 
-# Press the green button in the gutter to run the script.
+class Front(Widget):
+    pass
+
+
+class MainApp(App):
+
+    def build(self):
+        return Front()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    MainApp().run()
