2f8229988d40d5cae03d40b55cc9474a58ef4325
Nikita Zhuravlev <juravlev.nv@gmail.com>
2021-09-22 02:21:48 +0300

    Interface with setting panel, naked base

--- main.py
+++ main.py
@@ -1,18 +1,50 @@
 import kivy
 from kivy.app import App
-from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+from settings_json import settings_json
 
 kivy.require('2.0.0')
 
+# Set ratio
+Window.size = (1920 / 2, 1080 / 2)
 
-class Front(Widget):
+
+# Run fullscreen
+# Window.fullscreen = 'auto'
+# Or
+# Window.fullscreen = True
+# Window.borderless = False
+
+
+class Interface(BoxLayout):
     pass
 
 
 class MainApp(App):
 
     def build(self):
-        return Front()
+        Window.bind(on_keyboard=self.key_action)
+        return Interface()
+
+    # Shortcut for settings
+    def key_action(self, window, key, scancode, codepoint, modifier):
+        if modifier == ['shift'] and codepoint == 'm':
+            self.open_settings()
+
+    # Custom settings
+    def build_config(self, config):
+        config.setdefaults('promo', {
+            'path': '/images/'})
+
+    def build_settings(self, settings):
+        settings.add_json_panel('General',
+                                self.config,
+                                data=settings_json
+                                )
+
+    def on_config_change(self, config, section, key, value):
+        pass
 
 
 if __name__ == '__main__':
---
+++ settings_json.py
@@ -0,0 +1,16 @@
+import json
+
+settings_json = json.dumps([
+    {
+        'type': 'title',
+        'title': 'Promo'
+
+    },
+    {
+        'type': 'path',
+        'title': 'Path',
+        'desc': 'Choose image file',
+        'section': 'promo',
+        'key': 'path'
+    }
+])
