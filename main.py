7999ba715b85d1cf3c63d4a45f51f3b55e93d22d
Nikita Zhuravlev <juravlev.nv@gmail.com>
2021-09-22 21:38:28 +0300

    Added settings, images can be changed under settings and saved for next launch

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
 from settings_json import settings_json
+from kivy.uix.image import Image
 
 kivy.require('2.0.0')
 
@@ -16,6 +17,16 @@ Window.size = (1920 / 2, 1080 / 2)
 # Window.fullscreen = True
 # Window.borderless = False
 
+# Custom
+class ImageWithConfig(Image):
+    def check_path_promo(self):
+        self.source = App.get_running_app().config.get('Promo', 'promo_path')
+        return self.source
+
+    def check_path_bg(self):
+        self.source = App.get_running_app().config.get('Main', 'bg_path')
+        return self.source
+
 
 class Interface(BoxLayout):
     pass
@@ -24,6 +35,10 @@ class Interface(BoxLayout):
 class MainApp(App):
 
     def build(self):
+        # Load config, remove default
+        self.use_kivy_settings = False
+        settings = self.config.items('Promo', 'Main')
+        # Bind key action
         Window.bind(on_keyboard=self.key_action)
         return Interface()
 
@@ -34,17 +49,25 @@ class MainApp(App):
 
     # Custom settings
     def build_config(self, config):
-        config.setdefaults('promo', {
-            'path': '/images/'})
+        config.setdefaults('Promo', {
+            'promo_path': '...',
+        })
+        config.setdefaults('Main', {
+            'title_main': '',
+            'bg_path': '...',
+            'vk_link': 'vk.com/',
+            'insta_link': 'instagram.com/'
+        })
 
     def build_settings(self, settings):
-        settings.add_json_panel('General',
+        settings.add_json_panel('Settings',
                                 self.config,
                                 data=settings_json
                                 )
 
     def on_config_change(self, config, section, key, value):
-        pass
+        self.root.ids.image_promo.check_path_promo()
+        self.root.ids.image_bg.check_path_bg()
 
 
 if __name__ == '__main__':
--- settings_json.py
+++ settings_json.py
@@ -8,9 +8,42 @@ settings_json = json.dumps([
     },
     {
         'type': 'path',
-        'title': 'Path',
-        'desc': 'Choose image file',
-        'section': 'promo',
-        'key': 'path'
-    }
+        'title': 'Promo picture',
+        'desc': 'Choose promotional picture',
+        'section': 'Promo',
+        'key': 'promo_path'
+    },
+    {
+        'type': 'title',
+        'title': 'Main'
+
+    },
+    {
+        'type': 'string',
+        'title': 'Title',
+        'desc': 'Shortly describe your promotional offer',
+        'section': 'Main',
+        'key': 'title_main'
+    },
+    {
+        'type': 'path',
+        'title': 'Background picture',
+        'desc': 'Choose background image',
+        'section': 'Main',
+        'key': 'bg_path'
+    },
+    {
+        'type': 'string',
+        'title': 'VKontakte',
+        'desc': 'Insert link to VKontakte page',
+        'section': 'Main',
+        'key': 'vk_link'
+    },
+    {
+        'type': 'string',
+        'title': 'Instagram',
+        'desc': 'Insert link to Instagram page',
+        'section': 'Main',
+        'key': 'insta_link'
+    },
 ])
